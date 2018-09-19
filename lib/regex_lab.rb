b2e1869cf6f7ed7a12231427e96a32e0f8bf3294
Saint-Jean Germila <germila@noukod.com>
2018-09-19 13:50:40 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
+  word = ["afoot",  "Excellent", "incredible",  "Absolute",  "unreal",  "Inconceivable", "octopus", "chandoo",  "Crenel", "crooked", "Flimsy", "folksy", "Dog", "Hermit", "makeshift", "Palazzi", "sixfold", "Truth"]
+  word.match(/\w+aeiou/)#.(/^\w{1}\s/)
+  true
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
+#def words_starting_with_un_and_ending_with_ing(text)
+#text = ["unassuming", "ambiguous", "understanding", "pomp", "circumstance", "uninteresting", "uncompromising", "grouchy", "corollary"]
+#text.scan(/\w+ain/)
+#end
 
-end
+#def words_five_letters_long(text)
+#text = [ "extreme", "briny", "crepe", "parking", "snaps", "grouping", "snafu", "round", "dog", "be", "fork", "spoon"]
+#text.grep(/^\w{5}\s/)
+#end
 
-def words_five_letters_long(text)
+#def first_word_capitalized_and_ends_with_punctuation?(text)
+#text = ["I stepped on a Corn Flake, now I'm a cereal killer.", "i wondered why the baseball was getting bigger. Then it hit me.", "Did you hear about the guy whose whole left side was cut off? He's all right now", "when fish are in schools, they sometimes take debate"]
 
-end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
+#end
 
-end
+#def valid_phone_number?(phone)
+#  phone = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134", "28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
 
-def valid_phone_number?(phone)
 
-end
+#end
